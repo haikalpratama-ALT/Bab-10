8e8be714b7f41d3a529a36323d3ec7362cdb4be5
haikalpratama-ALT <haikalpratama@mail.ugm.ac.id>
2025-11-26 09:57:56 +0700

    Update Latihan 2.cpp

--- Latihan 2.cpp
+++ Latihan 2.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <algorithm>
 using namespace std;
+
+void bubbleSort(int A[], int N) {
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - i - 1; j++) {
+            if (A[j] > A[j + 1]) {
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
+        }
+    }
+}
 
 int cari_biner(int data[], int s, int cari) {
     int a, b, t, langkah;
@@ -18,17 +29,16 @@ int cari_biner(int data[], int s, int cari) {
 
         if (data[t] == cari) {
             ketemu = true;
+        } else if (data[t] > cari) {
+            b = t - 1;
         } else {
-            if (data[t] > cari) {
-                b = t - 1;
-            } else {
-                a = t + 1;
-            }
+            a = t + 1;
         }
         langkah++;
+
     } while ((a <= b) && !(ketemu));
 
-    return ketemu ? langkah : -1 * langkah;
+    return ketemu ? langkah : -langkah;
 }
 
 int main() {
@@ -41,16 +51,15 @@ int main() {
         A[i] = rand() % 10000 + 1;
     }
 
-    sort(A, A + N);
+    bubbleSort(A, N);
 
-    cout << "Isi Array Setelah Diurutkan :  ";
+    cout << "Isi Array Setelah Diurutkan:\n";
     for (int i = 0; i < N; i++) {
         cout << A[i] << "\t";
-        if ((i + 1) % 20 == 0)
-            cout << endl;
+        if ((i + 1) % 20 == 0) cout << endl;
     }
 
-    cout << "===============================================";
+    cout << "\n===============================================\n";
 
     int cari;
     cout << "Masukkan angka yang ingin dicari: ";
@@ -59,13 +68,12 @@ int main() {
     int hasil = cari_biner(A, N, cari);
 
     if (hasil > 0) {
-        cout << "Data ditemukan!" << endl;
+        cout << "Data ditemukan\n";
         cout << "Jumlah langkah pencarian = " << hasil << endl;
     } else {
-        cout << "Data TIDAK ditemukan." << endl;
+        cout << "Data tidak ditemukan\n";
         cout << "Jumlah langkah pencarian = " << -hasil << endl;
     }
 
     return 0;
 }
-
